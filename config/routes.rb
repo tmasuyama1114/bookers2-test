a00394bbe4fe1c7b3670207cd68cad8822667fcc
tmasuyama1114 <tmasuyama1114@gmail.com>
2024-05-29 10:43:04 +0900

    Add favorite feature to books

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
 
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+
   resources :users, only: [:index,:show,:edit,:update]
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
